d3f3b633ecaa30b2315c09fe6db2b1b61b59df7e
Anton Goroshko <radjahair@gmail.com>
2020-12-06 11:50:54 +1000

    Add games#index

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,6 @@
 class Word < ApplicationRecord
   has_many :games, dependent: :destroy
 
-  validates :text, presence: true, uniqueness: true, format: {with: /\A\p{Alpha}\z/}
+  validates :text,
+            presence: true, uniqueness: true, format: { with: /\A\p{Alpha}+\z/ }
 end
