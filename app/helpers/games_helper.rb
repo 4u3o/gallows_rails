847274b675c50909ef4736eccf007b084b93c765
Anton Goroshko <radjahair@gmail.com>
2020-12-06 13:01:03 +1000

    Add games#show

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,18 @@
 module GamesHelper
+  def word_to_show(game)
+    result =
+      game.letters_to_guess.map do |letter|
+        if letter.nil?
+          '__'
+        else
+          letter
+        end
+      end
+
+    result.join(' ')
+  end
+
+  def errors_to_show(game)
+    game.errors_letters.join(' ')
+  end
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -3,4 +3,8 @@ class Word < ApplicationRecord
 
   validates :text,
             presence: true, uniqueness: true, format: { with: /\A\p{Alpha}+\z/ }
+
+  def letters
+    text.chars
+  end
 end
